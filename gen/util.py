26c1f93c293b6e9b25ba3d69bdc3423ac64f9771
Andrey Malets <andrey.malets@gmail.com>
2013-06-10 17:48:16 +0600

    filter rdns and fb nets

--- gen/util.py
+++ gen/util.py
@@ -1,5 +1,15 @@
 from cmd import add_cmd
 
-@add_cmd('list_nets', False, 0)
+def filter_nets(state, prop):
+    return filter(lambda net: prop in net.props, state.networks)
+
+def fmt_nets(nets):
+    return '\n'.join(map(lambda net: net.get_addr(), nets))
+
+@add_cmd('list_rdns_nets', False, 0)
 def list_nets(state):
-    return '\n'.join(map(lambda net: net.get_addr(), state.networks))
+    return fmt_nets(filter_nets(state, 'rdns'))
+
+@add_cmd('list_fb_nets', False, 0)
+def list_nets(state):
+    return fmt_nets(filter_nets(state, 'fb'))
