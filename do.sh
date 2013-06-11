e31f6ef7c26f698452bcfaab8cc49570779b410a
root <root@dijkstra.urgu.org>
2013-06-12 03:14:32 +0600

    python2.6 compat, add dhcp update script

--- do.sh
+++ do.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 BASE=`dirname $0`
 MAIN="python ./main.py"
@@ -7,8 +7,23 @@ DATA=/var/lib/cfg
 export CFG=$BASE/cfg/conf.yaml
 
 gen_dhcp() {
-    CONF=/etc/dhcp/dhcpd.conf
-    $MAIN dhcp $BASE/cfg/dhcp.template > $CONF.new
+    local CUR=/etc/dhcp/dhcpd.conf
+    local NEW=$DATA/dhcpd.conf
+    $MAIN dhcp $BASE/cfg/dhcp.template > $NEW
+    local rv=$?
+    if [ $rv -eq 0 ]; then
+        cmp -s $CUR $NEW
+        if [ $? -ne 0 ]; then
+            mv $NEW $CUR
+            /etc/init.d/isc-dhcp-server restart
+        else
+            rm $NEW
+        fi
+        exit $?
+    else
+        exit $rv
+    fi
 }
 
 mkdir -p $DATA
+gen_dhcp
--- parse/host.py
+++ parse/host.py
@@ -7,8 +7,8 @@ def get_sname(name): return name.split('.')[0]
 class Host:
     @staticmethod
     def expand_mac(mac):
-        return '{}:{}:{}:{}:{}:{}'.format(mac[0:2], mac[2:4],  mac[4:6],
-                                          mac[6:8], mac[8:10], mac[10:12])
+        return '{0}:{1}:{2}:{3}:{4}:{5}'.format(mac[0:2], mac[2:4],  mac[4:6],
+                                                mac[6:8], mac[8:10], mac[10:12])
     def __init__(self, data, defaults):
         names = data.pop(0)
         if type(names) == list:
