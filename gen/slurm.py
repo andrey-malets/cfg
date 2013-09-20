b0a140ba76b8d1a17a5bac2eff7161d0be70f661
root <root@dijkstra.urgu.org>
2013-09-20 23:58:09 +0600

    collect puppet facts for slurm and redistribute them to nodes

--- gen/slurm.py
+++ gen/slurm.py
@@ -1,11 +1,6 @@
 import jinja2, json
 from cmd import add_cmd
 
-def parse_mem(pretty):
-    multipliers = { 'MB': 1, 'GB': 1024 }
-    volume, suffix = pretty.split(' ')
-    return int(float(volume) * multipliers[suffix]) - 10
-
 @add_cmd('slurm', True, 1)
 def gen_slurm(state, template, facts_path):
     for host in state.hosts:
@@ -28,5 +23,4 @@ def gen_slurm(state, template, facts_path):
 
     return jinja2.Template(template).render(hosts=hosts,
                                             groups=groups,
-                                            default=default,
-                                            parse_mem=parse_mem)
+                                            default=default)
