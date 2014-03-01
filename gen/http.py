fbd5cb388e20beffc57be91c5d0bca377eaf61a6
root <root@dijkstra.urgu.org>
2014-03-01 22:16:44 +0600

    add first version of HTTP backend config

--- gen/http.py
+++ gen/http.py
@@ -4,3 +4,7 @@ from cmd import add_cmd
 @add_cmd('ext_http', True, 0)
 def gen_ext_http(state, template):
     return jinja2.Template(template).render(state=state)
+
+@add_cmd('http_back', True, 0)
+def gen_http_back(state, template):
+    return jinja2.Template(template).render(state=state)
