ef17f5f22aecf3ccc78181c0af209faaaccf993c
root <root@dijkstra.urgu.org>
2014-03-03 13:11:48 +0600

    add optional auto update facility

--- puppet/modules/cron-apt/manifests/init.pp
+++ puppet/modules/cron-apt/manifests/init.pp
@@ -8,4 +8,14 @@ class cron-apt {
         content => template('cron-apt/config.erb'),
         require => Package['cron-apt'],
     }
+
+    if $autoupdate {
+        file { '/etc/cron-apt/action.d/3-download':
+            mode => 644,
+            owner => root,
+            group => root,
+            source => 'puppet:///modules/cron-apt/3-download',
+            require => Package['cron-apt'],
+        }
+    }
 }
