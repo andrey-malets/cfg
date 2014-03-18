fbada965c74b623a771f1e1f9f5512783ef16b54
root <root@dijkstra.urgu.org>
2014-03-18 23:21:22 +0600

    move DNS generation logic back to python

--- gen/dns.py
+++ gen/dns.py
@@ -9,16 +9,16 @@ def get_rname(host, network):
 
 def dns_fn(fn):
     return (lambda state, template, serial, *args:
-        jinja2.Template(template).render(hosts=fn(state, *args), serial=serial))
+        jinja2.Template(template).render(records=fn(state, *args), serial=serial))
 
 @add_cmd('dns', True, 2)
 @dns_fn
 def gen_fwd(state, zone):
     for host in state.hosts:
         if host.name.find('.') == host.name.find(zone) - 1:
-            yield {'name'    : host.sname,
-                   'addr'    : host.addr,
-                   'aliases' : host.saliases}
+            yield (host.sname, 'A', host.addr)
+            for alias in host.saliases:
+                yield (alias, 'CNAME', host.sname)
 
 @add_cmd('rdns', True, 2)
 @dns_fn
@@ -27,7 +27,7 @@ def gen_reverse(state, net_name):
     assert network.is_classful(), ('not supported for classless mask /%d' % network.count)
     for host in filter(lambda host: host.addr != None, state.hosts):
         if network.has(host.addr):
-            yield {'name' : host.name, 'addr' : get_rname(host, network)}
+            yield (get_rname(host, network), 'PTR', host.name + '.')
 
 @add_cmd('dns_fb', True, 2)
 @dns_fn
@@ -36,6 +36,8 @@ def gen_fb(state, net_name):
     for host in state.hosts:
         for addr in host.fb:
             if network.has(addr):
-                yield {'name'    : host.sname,
-                       'addr'    : addr,
-                       'aliases' : host.saliases}
+                pass
+                #TODO(malets):
+#                yield {'name'    : host.sname,
+#                       'addr'    : addr,
+#                       'aliases' : host.saliases}
