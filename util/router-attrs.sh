fcd074c6eecfd4436155ea0c1544d6276de977aa
root <root@dijkstra.urgu.org>
2014-03-29 02:36:25 +0600

    generalize router attributes utility

--- util/router-attrs.sh
+++ util/router-attrs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-res=($(ip route | grep src | while read route ; do
+attr=${1?no attribute specified}
+
+res=($(ip route | grep "$attr" | while read route ; do
     tokens=($route)
     dst=${tokens[0]}
     for i in $(seq 1 2 ${#tokens[*]}); do
-        if [ "${tokens[i]}" = src ]; then
+        if [ "${tokens[i]}" = "$attr" ]; then
             echo $dst:${tokens[i+1]}
         fi
     done
