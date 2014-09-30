9cb33682cdafb191150ea547c6cf23c6b2ed189d
root <root@dijkstra.urgu.org>
2014-09-30 23:58:57 +0600

    Proper UPS monitoring on servers

---
+++ puppet/modules/ups/manifests/init.pp
@@ -0,0 +1,29 @@
+class ups {
+    package { 'apcupsd':
+        ensure => installed,
+    }
+
+    service { 'apcupsd':
+        ensure => 'running',
+        require => Package['apcupsd'],
+    }
+
+    file { '/etc/apcupsd/apcupsd.conf':
+        notify  => Service['apcupsd'],
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        content => template('ups/apcupsd.conf.erb'),
+        require => Package['apcupsd'],
+    }
+
+    file { '/etc/default/apcupsd':
+        notify  => Service['apcupsd'],
+        mode  => 644,
+        owner => root,
+        group => root,
+        content => "APCACCESS=/sbin/apcaccess
+                    ISCONFIGURED=yes
+                   ",
+    }
+}
