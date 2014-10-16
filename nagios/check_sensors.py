50a0fc29e673fae14434a769aa81d4a07d719566
root <root@dijkstra.urgu.org>
2014-10-16 18:08:58 +0600

    #22: use globs in sensors names for convenience

--- nagios/check_sensors.py
+++ nagios/check_sensors.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, sys
-
-
-PREFIX='/sys/class/hwmon'
+import glob, os, re, sys
 
 def get_devices():
+    PREFIX='/sys/class/hwmon'
     rv = {}
     for comp in os.listdir(PREFIX):
-        devpath = '%s/%s/device/' % (PREFIX, comp)
+        devpath = '%s/%s/device' % (PREFIX, comp)
         namepath = '%s/name' % devpath
         if os.path.isfile(namepath):
             with open(namepath) as namefile:
@@ -37,25 +35,33 @@ def format_value(name, value):
             return fmt % value
     assert(False)
 
-def check_sensor(devpath, devname, name, values):
-    senspath = '%s/%s_input' % (devpath, name)
+def check_sensor(devpath, devname, patt, values):
+    senspatt = '%s/%s_input' % (devpath, patt)
     (cmin, wmin, wmax, cmax) = map(int, values.split(','))
-    if not os.path.isfile(senspath):
-        return (2, 'did not find %s for %s!' % (devname, name))
-    value = None
-    with open (senspath) as sensor:
-        raw_value = sensor.read()
-        value = normalize_value(name, raw_value)
-        if value == None:
-            return (2, 'cant normalize value "%s"' % raw_value)
-    assert(value)
-    formatted_value = '%s:%s: %s' % (devname, name, format_value(name, value))
-    status = 0
-    if value < cmin or value > cmax:
-        status = 2
-    elif value < wmin or value > wmax:
-        status = 1
-    return (status, formatted_value)
+
+    sensors = glob.glob(senspatt)
+    if len(sensors) == 0:
+        return [(2, 'no sensors found matching %s:%s!' % (devname, patt))]
+    results = []
+    for senspath in sensors:
+        assert(os.path.isfile(senspath))
+        value = None
+        with open (senspath) as sensor:
+            raw_value = sensor.read().strip()
+            value = normalize_value(patt, raw_value)
+            if value == None:
+                results.append((2, 'cant normalize value "%s"' % raw_value))
+                continue
+        assert(value)
+        name = re.match('%s/(.+)_input' % devpath, senspath).group(1)
+        formatted_value = '%s:%s: %s' % (devname, name, format_value(patt, value))
+        status = 0
+        if value < cmin or value > cmax:
+            status = 2
+        elif value < wmin or value > wmax:
+            status = 1
+        results.append((status, formatted_value))
+    return results
 
 if __name__ == '__main__':
     if len(sys.argv) != 2 or len(sys.argv[0]) == 0:
@@ -66,9 +72,9 @@ if __name__ == '__main__':
     specs = sys.argv[1].split('+')
     for spec in specs:
         try:
-            devname, sensorname, values = spec.split(':')
+            devname, patt, values = spec.split(':')
             sensors = matching.get(devname, [])
-            sensors.append((sensorname, values))
+            sensors.append((patt, values))
             matching[devname] = sensors
         except:
             print 'CRITITAL: could not parse command line'
@@ -79,10 +85,10 @@ if __name__ == '__main__':
 
     for devpath, name in get_devices().iteritems():
         if name in matching:
-            for sensorname, values in matching[name]:
-                status, message = check_sensor(devpath, name, sensorname, values)
-                results[status].append(message)
-                all_status = max(all_status, status)
+            for patt, values in matching[name]:
+                for status, message in check_sensor(devpath, name, patt, values):
+                    results[status].append(message)
+                    all_status = max(all_status, status)
 
     messages = ['OK', 'WARNING', 'CRITICAL']
     print 'Sensors %s: %s' % (messages[all_status], ', '.join(results[all_status]))
