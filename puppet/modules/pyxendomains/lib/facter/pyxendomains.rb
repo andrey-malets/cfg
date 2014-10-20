ab50275a001187ec31a5fd7bdf2efa61d7bbb720
root <root@dijkstra.urgu.org>
2014-10-20 23:47:41 +0600

    Use different path for xendomains.py

--- puppet/modules/pyxendomains/lib/facter/pyxendomains.rb
+++ puppet/modules/pyxendomains/lib/facter/pyxendomains.rb
@@ -1,6 +1,6 @@
 Facter.add("pyxendomains") do
   confine :virtual => 'xen0'
   setcode do
-    Facter::Util::Resolution.exec('/var/lib/puppet/lib/facter/util/xendomains.py')
+    Facter::Util::Resolution.exec('/usr/local/bin/xendomains.py')
   end
 end
