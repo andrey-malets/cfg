47a9597847862fec3ec395b4d0caf27c03ebd9b5
root <root@dijkstra.urgu.org>
2014-12-17 01:45:34 +0500

    Added backup script for Debian-based systems

---
+++ puppet/files/backup.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -e
+
+build_findcmd() {
+    local paths=(boot
+                 lib/modules vmlinuz initrd.img
+                 dev proc run sys tmp
+                 home root
+                 var/{cache,log,local,spool}
+                 var/lib/{apt{,itude},dkms,dpkg,gems,nagios3/spool,puppet})
+    local exts=(d o pyc)
+    local rv= start=1
+    for path in "${paths[@]}"; do
+        cond="-path /$path -prune"
+        [[ -z "$start" ]] && rv="$rv -o $cond" || rv="$rv $cond"
+        start=
+    done
+    for ext in "${exts[@]}"; do rv="$rv -o -name *.$ext"; done
+    echo "find / $rv -o -type f -a -print0"
+}
+
+pkgs() {
+    dpkg-query -W -f '${Package}:${Architecture}\n'
+}
+
+conffiles() {
+    all_pkgs=($(pkgs))
+    declare -A conffiles
+    for spec in $(dpkg-query -f '${Conffiles}\n' -W "${all_pkgs[@]}" | \
+                  egrep -v '^[[:space:]]*$' | awk '{print $1":"$2}'); do
+        if [[ -r "${spec%%:*}" ]]; then
+            conffiles[${spec%%:*}]=${spec##*:}
+        fi
+    done
+
+    tar cf - -T <(md5sum "${!conffiles[@]}" | while read sum conffile; do
+        if [[ "${conffiles[$conffile]}" != "$sum" ]]; then
+            echo "$conffile"
+        fi
+    done)
+}
+
+systemfiles() {
+    all_pkgs=($(pkgs))
+    declare -A allfiles
+    for file in $(dpkg-query -L "${all_pkgs[@]}" | \
+                  egrep -v '^[[:space:]]*$'); do
+        allfiles[$file]=1
+    done
+
+    findcmd=$(build_findcmd)
+
+    tar cf - -T <(while read -r -d ''; do
+        if [[ -z "${allfiles[$REPLY]}" ]]; then
+            echo "$REPLY"
+        fi
+    done < <($findcmd))
+}
+
+userfiles() {
+    tar cf - /home /root
+}
+
+case "$1" in
+    pkgs) pkgs ;;
+    conf) conffiles ;;
+    sys)  systemfiles ;;
+    user) userfiles ;;
+    *)    exit 1 ;;
+esac
