99e9229ef85e593af70e71e0a37c968eda1b4994
root <root@dijkstra.urgu.org>
2014-12-21 23:52:13 +0500

    Do not backup /data in system files backup step

--- puppet/files/backup.sh
+++ puppet/files/backup.sh
@@ -7,7 +7,7 @@ exec_findcmd() {
                  lib/modules vmlinuz initrd.img
                  etc/{blkid.,fs}tab
                  dev proc run sys tmp
-                 home place root
+                 data home place root
                  usr/share/{mime,snmp}
                  var/{cache,log,local,spool,tmp}
                  var/lib/{apt{,itude},dkms,dpkg,gems,nagios3/spool,puppet})
