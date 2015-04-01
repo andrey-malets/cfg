da75c7bb27b88dc2d4800a3d027fb5658be8b1a5
root <root@dijkstra.urgu.org>
2015-04-02 02:40:32 +0500

    Refactor backup{,er}.sh

--- puppet/files/backup.sh
+++ puppet/files/backup.sh
@@ -51,10 +51,6 @@ find_userfiles() {
     find /home "${common_findopts[@]}"
 }
 
-manual_pkgs() {
-    apt-mark showmanual
-}
-
 conffiles() {
     all_pkgs=($(all_pkgs))
     declare -A conffiles
@@ -119,62 +115,10 @@ diff_backup() {
     )
 }
 
-userfiles() {
-    "${tarcmd[@]}" home root
-}
-
-remote_backup() {
-    local rhost=$1 rcmd=$2 sumfile=$3 output=$4
-    [[ -f "$sumfile" ]] || touch "$sumfile"
-    ssh "$rhost" "$0" "$rcmd" < "$sumfile" > "$output"
-}
-
-update() {
-    local sumfile=$1 backup=$2
-
-    declare -A md5sums
-    while read -r -d ''; do
-        md5sum=${REPLY%% *}
-        file=${REPLY#* }
-        md5sums[$file]=$md5sum
-    done < "$sumfile"
-
-    script='
-import md5, sys, tarfile
-
-with tarfile.open(sys.argv[1]) as tfile:
-    for member in tfile:
-        md5sum = md5.new()
-        memberfile = tfile.extractfile(member)
-        while True:
-            data = memberfile.read(4096)
-            if len(data) == 0:
-                break
-            md5sum.update(data)
-        sys.stdout.write("{} {}\0".format(md5sum.hexdigest(),
-                                          member.name))'
-    while read -r -d ''; do
-        md5sum=${REPLY%% *}
-        file=${REPLY#* }
-        md5sums[$file]=$md5sum
-    done < <(python -c "$script" "$backup")
-
-    for file in "${!md5sums[@]}"; do
-        echo -en "${md5sums[$file]} $file\0"
-    done > "$sumfile.new"
-
-    mv "$sumfile.new" "$sumfile"
-}
-
 case "$1" in
-    pkgs) manual_pkgs ;;
+    pkgs) apt-mark showmanual ;;
     conf) conffiles ;;
-
     sys)  collect_systemfiles; diff_backup find_systemfiles not_systemfile ;;
     user) diff_backup find_userfiles true ;;
-
-    remote_backup) shift; remote_backup "$@" ;;
-    update)        shift; update "$@" ;;
-
     *)    exit 1 ;;
 esac
--- puppet/files/backuper.sh
+++ puppet/files/backuper.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-if [[ "$#" -lt 2 ]]; then
-    echo "usage: $0 <host> {full,diff} <data> [data...]" 1>&2
-    exit 1
-fi
-
-host=$1; shift
-
-backup="/usr/local/bin/backup.sh"
-remote=(ssh "$host" -l root "$backup")
-
 ppattern=%F_%H-%M-%S
 spattern=????-??-??_??-??-??
 fullprefix=full
@@ -29,72 +19,41 @@ last_fullname() {
     fi
 }
 
-backup_pkgs() {
-    "${remote[@]}" pkgs > "$1"
-}
+update() {
+    local sumfile=$1 backup=$2
 
-backup_conffiles() {
-    "${remote[@]}" conf > "$1"
-}
+    declare -A md5sums
+    while read -r -d ''; do
+        md5sum=${REPLY%% *}
+        file=${REPLY#* }
+        md5sums[$file]=$md5sum
+    done < "$sumfile"
 
-backup_sysfiles() {
-    local sumfile=$1 output=$2
-    "$backup" remote_backup "root@$host" sys "$sumfile" "$output"
-    "$backup" update "$sumfile" "$output"
-}
+    script='
+import md5, sys, tarfile
 
-backup_userfiles() {
-    local sumfile=$1 output=$2
-    "$backup" remote_backup "root@$host" user "$sumfile" "$output"
-    "$backup" update "$sumfile" "$output"
-}
+with tarfile.open(sys.argv[1]) as tfile:
+    for member in tfile:
+        md5sum = md5.new()
+        memberfile = tfile.extractfile(member)
+        while True:
+            data = memberfile.read(4096)
+            if len(data) == 0:
+                break
+            md5sum.update(data)
+        sys.stdout.write("{} {}\0".format(md5sum.hexdigest(),
+                                          member.name))'
+    while read -r -d ''; do
+        md5sum=${REPLY%% *}
+        file=${REPLY#* }
+        md5sums[$file]=$md5sum
+    done < <(python -c "$script" "$backup")
 
-full_backup() {
-    local stamp=$(generate_timestamp)
-    local dest="$HOME/${fullprefix}_${stamp}"
-    if [[ -r "$dest" ]]; then
-        echo "$dest already exists, refusing to overwrite." >&2
-        exit 1
-    fi
+    for file in "${!md5sums[@]}"; do
+        echo -en "${md5sums[$file]} $file\0"
+    done > "$sumfile.new"
 
-    mkdir -p "$dest"
-    chmod 700 "$dest"
-
-    backup_pkgs "$dest/pkgs_$stamp"
-    backup_conffiles "$dest/conffiles_$stamp.tar"
-
-    for type in "$@"; do
-        case "$type" in
-            sys)  backup_sysfiles "$dest/sysfiles.md5sums" \
-                                  "$dest/sysfiles_${stamp}.tar" ;;
-            user) backup_userfiles "$dest/userfiles.md5sums" \
-                                   "$dest/userfiles_${stamp}.tar" ;;
-            *)   echo "unknown type: $type" 1>&2; exit 1 ;;
-        esac
-    done
-}
-
-diff_backup() {
-    local last_full=$(last_fullname "$HOME")
-    if [[ -z "$last_full" ]] || ! [[ -d "$last_full" ]]; then
-        echo 'No full backup exists, creating one now.' >&2
-        full_backup "$@"
-    else
-        local stamp=$(generate_timestamp)
-
-        backup_pkgs "$last_full/pkgs_$stamp"
-        backup_conffiles "$last_full/conffiles_$stamp.tar"
-
-        for type in "$@"; do
-            case "$type" in
-                sys)  backup_sysfiles "$last_full/sysfiles.md5sums" \
-                                      "$last_full/sysfiles_${stamp}.tar" ;;
-                user) backup_userfiles "$last_full/userfiles.md5sums" \
-                                       "$last_full/userfiles_${stamp}.tar" ;;
-                *)    echo "unknown type: $type" 1>&2; exit 1 ;;
-            esac
-        done
-    fi
+    mv "$sumfile.new" "$sumfile"
 }
 
 clean_older_than() {
@@ -119,12 +78,102 @@ print goal.strftime("%Y-%m-%d_%H-%M-%S")' "$@")"
     done
 }
 
-btype=$1
-shift
+remote_backup() {
+    local host=$1; shift
+    local backup_cmd="/usr/local/bin/backup.sh"
+    ssh -l root "$host" "$backup_cmd" "$@"
+}
 
+get_destination() {
+    local stamp=$1
+    echo "$HOME/${fullprefix}_${stamp}"
+}
+
+get_name() {
+    local type=$1 dest=$2 stamp=$3
+    case "$type" in
+        pkgs) fn="pkgs_$stamp" ;;
+        conf) fn="conffiles_$stamp.tar" ;;
+        sys)  fn="sysfiles_$stamp.tar" ;;
+        user) fn="userfiles_$stamp.tar" ;;
+        *)    echo "unknown type: $type" >&2; exit 1 ;;
+    esac
+    echo "$dest/$fn"
+}
+
+get_sumname() {
+    local type=$1 dest=$2
+    case "$type" in
+        sys)  fn=sysfiles ;;
+        user) fn=userfiles ;;
+        *)    echo "unknown type: $type" >&2; exit 1 ;;
+    esac
+    echo "$dest/$fn.md5sums"
+}
+
+backup_smallfiles() {
+    local host=$1 dest=$2 stamp=$3
+    for type in pkgs conf; do
+        remote_backup "$host" "$type" > "$(get_name "$type" "$dest" "$stamp")"
+    done
+}
+
+backup_with_sums() {
+    local host=$1 cmd=$2 sumfile=$3 output=$4
+    touch "$sumfile"
+    remote_backup "$host" "$cmd" < "$sumfile" > "$output"
+    update "$sumfile" "$output"
+}
+
+backup_bigfiles() {
+    local host=$1 dest=$2 stamp=$3; shift 3
+    for type in "$@"; do
+        case "$type" in
+            sys|user) backup_with_sums "$host" "$type" \
+                                       "$(get_sumname "$type" "$dest")" \
+                                       "$(get_name "$type" "$dest" "$stamp")" ;;
+            *)        echo "unknown type: $type" 1>&2; exit 1 ;;
+        esac
+    done
+}
+
+full_backup() {
+    local host=$1 stamp=$(generate_timestamp)
+    local dest=$(get_destination "$stamp")
+    shift
+
+    if [[ -r "$dest" ]]; then
+        echo "$dest already exists, refusing to overwrite." >&2
+        exit 1
+    fi
+
+    mkdir -p -m 700 "$dest"
+    backup_smallfiles "$host" "$dest" "$stamp"
+    backup_bigfiles "$host" "$dest" "$stamp" "$@"
+}
+
+diff_backup() {
+    local host=$1 last_full=$(last_fullname "$HOME")
+    shift
+    if [[ -z "$last_full" ]] || ! [[ -d "$last_full" ]]; then
+        echo 'No full backup exists, creating one now.' >&2
+        full_backup "$@"
+    else
+        local stamp=$(generate_timestamp)
+        backup_smallfiles "$host" "$last_full" "$stamp"
+        backup_bigfiles "$host" "$last_full" "$stamp" "$@"
+    fi
+}
+
+if [[ "$#" -lt 2 ]]; then
+    echo "usage: $0 <host> {full,diff} <data> [data...]" 1>&2
+    exit 1
+fi
+
+host=$1; btype=$2; shift 2
 case "$btype" in
-    full)  full_backup "$@" ;;
-    diff)  diff_backup "$@" ;;
+    full)  full_backup "$host" "$@" ;;
+    diff)  diff_backup "$host" "$@" ;;
     clean) clean_older_than "$@" ;;
-    *)    echo "unknown backup type: $btype" 1>&2; exit 1 ;;
+    *)     echo "unknown backup type: $btype" 1>&2; exit 1 ;;
 esac
