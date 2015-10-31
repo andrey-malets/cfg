f1e134623d59af2981052c83783da14b90605ad2
root <root@dijkstra.urgu.org>
2015-10-31 23:14:11 +0500

    Update install options and service definition for nrpe module.

--- puppet/modules/nrpe/manifests/init.pp
+++ puppet/modules/nrpe/manifests/init.pp
@@ -1,24 +1,12 @@
 class nrpe {
     package { 'nagios-nrpe-server':
         ensure => installed,
-        install_options => norecommended,
+        install_options => '--no-install-recommends',
     }
 
     package { 'nagios-plugins-basic':
         ensure => installed,
-        install_options => norecommended,
-    }
-
-    service { 'nagios-nrpe-server':
-        ensure     => 'running',
-        enable     => 'true',
-        hasrestart => 'true',
-        hasstatus  => 'false',
-        restart    => '/etc/init.d/nagios-nrpe-server restart',
-        pattern    => '/usr/sbin/nrpe',
-        require => [Package['nagios-nrpe-server'],
-                    Package['nagios-plugins-basic'],
-                    File['/etc/nagios/nrpe.cfg']],
+        install_options => '--no-install-recommends',
     }
 
     file { '/etc/nagios/nrpe.cfg':
@@ -29,6 +17,14 @@ class nrpe {
         content => template('nrpe/nrpe.cfg.erb'),
         require => [Package['nagios-nrpe-server'],
                     Package['nagios-plugins-basic']],
+    }
+
+    service { 'nagios-nrpe-server':
+        ensure     => 'running',
+        enable     => 'true',
+        require => [Package['nagios-nrpe-server'],
+                    Package['nagios-plugins-basic'],
+                    File['/etc/nagios/nrpe.cfg']],
     }
 
     if $check_megaraid {
