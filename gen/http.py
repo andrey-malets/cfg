f5f4f04369588e9cf96e9203c7c60184b53c0b89
Andrey Malets <andrey.malets@gmail.com>
2020-09-30 20:38:06 +0500

    Remove keypath param from gen/http.py.

--- gen/http.py
+++ gen/http.py
@@ -1,9 +1,9 @@
 import jinja2
 from cmd import add_cmd
 
-@add_cmd('ext_http', True, 1)
-def gen_ext_http(state, template, keypath):
-    return jinja2.Template(template).render(state=state, keypath=keypath)
+@add_cmd('ext_http', True, 0)
+def gen_ext_http(state, template):
+    return jinja2.Template(template).render(state=state)
 
 @add_cmd('http_back', True, 1)
 def gen_http_back(state, template, keypath):
